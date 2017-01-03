6605d274295de90d2aaa665b03779214c4a728e1
impresyjna <joanna.furmaniak@student.put.poznan.pl>
2017-01-03 12:59:55 +0100

    Commit

--- src/main/java/bsr/server/innerServices/AccountService.java
+++ src/main/java/bsr/server/innerServices/AccountService.java
@@ -6,10 +6,13 @@ import bsr.server.exceptions.SessionException;
 import bsr.server.exceptions.UserException;
 import bsr.server.models.Account;
 import bsr.server.models.User;
+import bsr.server.models.accountOperations.Operation;
 
 import javax.annotation.Resource;
 import javax.jws.WebMethod;
+import javax.jws.WebParam;
 import javax.jws.WebService;
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.ws.BindingType;
 import javax.xml.ws.WebServiceContext;
 import java.sql.SQLException;
@@ -51,5 +54,43 @@ public class AccountService {
             return accounts;
         }
         return null;
+    }
+
+    @WebMethod
+    public Operation depositMoney(@WebParam(name = "title") @XmlElement(required = true) final String title,
+                                  @WebParam(name = "amount") @XmlElement(required = true) final double amount,
+                                  @WebParam(name = "targetAccountNumber") @XmlElement(required = true) final String targetAccountNumber) {
+        Map<String, Object> parametersMap = new HashMap<String, Object>() {{
+            put("title", title);
+            put("amount", amount);
+            put("receiver account no", targetAccountNumber);
+        }};
+        ValidateParamsUtil.validate(parametersMap);
+
+        Datastore datastore = DataStoreHandlerUtil.getInstance().getDataStore();
+        User user = AuthUtil.getUserFromWebServiceContext(context, datastore);
+
+        BankAccount sourceBankAccount = datastore.find(BankAccount.class).field("accountNo").equal(sourceAccountNo).get();
+        if(sourceBankAccount == null) {
+            throw new BankServiceException("Source bank account does not exist");
+        }
+        if(!user.containsBankAccount(sourceAccountNo)) {
+            throw new BankServiceException("Source account does not belong to user");
+        }
+
+        Transfer outTransfer = new Transfer(title, amount, sourceAccountNo, targetAccountNo, Transfer.TransferDirection.OUT);
+
+        if(targetAccountNo.substring(2, 10).equals(ConstantsUtil.BANK_ID)) {
+            BankAccount targetBankAccount = datastore.find(BankAccount.class).field("accountNo").equal(targetAccountNo).get();
+            if(targetBankAccount == null) {
+                throw new BankServiceException("Target bank account does not exist");
+            }
+            Transfer inTransfer = new Transfer(title, amount, sourceAccountNo, targetAccountNo, Transfer.TransferDirection.IN);
+            makeInternalTransfer(datastore, sourceBankAccount, targetBankAccount, inTransfer, outTransfer);
+        } else {
+            makeExternalTransfer(datastore, sourceBankAccount, outTransfer);
+        }
+
+        return outTransfer;
     }
 }
